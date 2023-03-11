025e75488bd6ddd76cfc204c648e33dd9970f80c
The Dumb Terminal <github@thedumbterminal.co.uk>
2023-03-11 18:20:36 +0000

    working for gas consumption too

--- src/main.py
+++ src/main.py
@@ -2,7 +2,18 @@ import functions_framework
 from octopus_energy_client import OctopusEnergy, ResourceType, ChargeType, Aggregate
 from datetime import datetime, timedelta, date
 
+octopus_client = OctopusEnergy()
 
+def consumption_from_response(response):
+    return response['results'][0]['consumption']
+
+def elec_for_day(when):
+    electricity_consumption = octopus_client.get_consumption_for_date(ResourceType.ELECTRICITY, when, group_by=Aggregate.DAILY)
+    return consumption_from_response(electricity_consumption)
+
+def gas_for_day(when):
+    gas_consumption = octopus_client.get_consumption_for_date(ResourceType.GAS, when, group_by=Aggregate.DAILY)
+    return consumption_from_response(gas_consumption)
 
 
 @functions_framework.http
@@ -13,25 +24,14 @@ def entry_http(request):
     
     request_args = request.args
     print(request_args)
-    
-    octopus_client = OctopusEnergy(
-        api_key=octopus_api_key,
-        electricity_serial=octopus_electricity_serial,
-        electricity_mpan=octopus_electricity_mpan,
-        electricity_product_code=octopus_electricity_product_code,
-        electricity_region=octopus_electricity_region,
-        gas_serial=octopus_gas_serial,
-        gas_mprn=octopus_gas_mprn,
-        gas_product_code=octopus_gas_product_code,
-        gas_region=octopus_gas_region,
-    )
 
     when = datetime.now() - timedelta(days = 1)
     if request_args['date']:
         when = date.fromisoformat(request_args['date'])
     print('Running for date: ', when)
 
-    electricity_consumption = octopus_client.get_consumption_for_date(ResourceType.ELECTRICITY, when, group_by=Aggregate.DAILY)
+    elec_amount = elec_for_day(when)
+    gas_amount = gas_for_day(when)
 
+    return f'Elec amount: {elec_amount}, Gas amount: {gas_amount}'
 
-    return electricity_consumption
